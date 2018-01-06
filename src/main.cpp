495a27ea92848b80da48a4e5549947d13f3551e1
Julian Torreno <juliantorreno@protonmail.com>
2018-01-06 13:33:01 -0500

    Render suzanne.

--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,14 @@ auto main() -> int {
     ogld::window window("opengl-demo", 640_px * 480_px);
     
     ogld::renderer renderer;
+
     ogld::static_object cube{ogld::obj_mesh("../opengl-demo/res/models/cube.obj"), {ogld::texture()}, {0, 0, 0}};
+    ogld::static_object suzanne{ogld::obj_mesh("../opengl-demo/res/models/suzanne.obj"), {ogld::texture("../opengl-demo/res/textures/white.png")}, {0, 3, 0}};
     
     renderer.render_list.push_back(cube);
+    renderer.render_list.push_back(suzanne);
 
-    ogld::camera camera({2, 2, 2}, {0, 0, 0}, 90);
+    ogld::camera camera({2, 4, 4}, {0, 2, 0}, 90);
 
     while (true) {
         renderer(camera);
