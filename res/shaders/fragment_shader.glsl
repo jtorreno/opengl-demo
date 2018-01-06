85911fa5cf7918b9beddca73baed77c903cb2999
Julian Torreno <juliantorreno@protonmail.com>
2018-01-06 13:06:46 -0500

    Implement basic lighting calculations.

--- res/shaders/fragment_shader.glsl
+++ res/shaders/fragment_shader.glsl
@@ -1,10 +1,12 @@
 #version 330 core
 
 in vec2 texcoord_fs;
+in vec3 normal_fs;
+
 out vec4 color;
 
 uniform sampler2D sampler;
 
 void main() {
-    color = texture(sampler, texcoord_fs);
+    color = (vec4(0.15, 0.15, 0.15, 0.15) * texture(sampler, texcoord_fs)) + texture(sampler, texcoord_fs) * clamp(dot(vec3(0, 0, 1), normal_fs), 0, 1);
 }
--- res/shaders/vertex_shader.glsl
+++ res/shaders/vertex_shader.glsl
@@ -5,6 +5,7 @@ in vec3 normal;
 in vec2 texcoord;
 
 out vec2 texcoord_fs;
+out vec3 normal_fs;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -12,6 +13,7 @@ uniform mat4 projection;
 
 void main() {
     texcoord_fs = texcoord;
+    normal_fs = normal;
 
     vec4 v = vec4(position, 1);
     gl_Position = mat4(projection * view * model) * v;
